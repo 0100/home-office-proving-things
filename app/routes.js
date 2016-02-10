c01f5d811e918085725e4ca4239329248881b164
Mark Goddard <markgoddard@Marks-Air.home>
2016-02-10 12:11:26 +0000

    title tweak

--- app/routes.js
+++ app/routes.js
@@ -10,6 +10,10 @@ router.get('/examples/template-data', function (req, res) {
   res.render('examples/template-data', { 'name' : 'Foo' });
 });
 
+router.get('/income-proving', function (req, res) {
+  res.render('income-proving/results', {'global_header_text' : 'Home Office'});
+});
+
 router.get('/income-proving/results', function (req, res) {
   var nino = req.param('nino');
   var fromDate = req.param('from-day')+"/"+req.param('from-month')+"/"+req.param('from-year');
