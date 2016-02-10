c0ce42313ef69be84dfb52cfe2f9db6b29e899a4
Mark Goddard <markgoddard@Marks-Air.home>
2016-02-10 13:06:43 +0000

    title tweak

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {'global_header_text' : 'Home Office'});
 });
 
 // Example route: Passing data into a page
@@ -10,10 +10,14 @@ router.get('/examples/template-data', function (req, res) {
   res.render('examples/template-data', { 'name' : 'Foo' });
 });
 
+router.get('/income-proving', function (req, res) {
+  res.render('index', {'global_header_text' : 'Home Office'});
+});
+
 router.get('/income-proving/results', function (req, res) {
-  var nino = req.param('nino');
-  var fromDate = req.param('from-day')+"/"+req.param('from-month')+"/"+req.param('from-year');
-  var toDate = req.param('to-day')+"/"+req.param('to-month')+"/"+req.param('to-year');
+  var nino = req.query.nino;
+  var fromDate = req.query.from_day+"/"+req.query.from_month+"/"+req.query.from_year;
+  var toDate = req.query.to_day+"/"+req.query.to_month+"/"+req.query.to_year;
 
   res.render('income-proving/results', {'global_header_text' : 'Home Office', 'nino': nino, 'fromDate': fromDate, 'toDate': toDate});
 });
