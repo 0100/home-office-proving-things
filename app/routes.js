ef0fec9ce7b4d04be42945367819e92ee755480c
Mark Goddard <markgoddard@Marks-Air.home>
2016-02-10 12:07:44 +0000

    first prototype release

---
+++ app/routes.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  res.render('index');
+});
+
+// Example route: Passing data into a page
+router.get('/examples/template-data', function (req, res) {
+  res.render('examples/template-data', { 'name' : 'Foo' });
+});
+
+router.get('/income-proving/results', function (req, res) {
+  var nino = req.param('nino');
+  var fromDate = req.param('from-day')+"/"+req.param('from-month')+"/"+req.param('from-year');
+  var toDate = req.param('to-day')+"/"+req.param('to-month')+"/"+req.param('to-year');
+
+  res.render('income-proving/results', {'global_header_text' : 'Home Office', 'nino': nino, 'fromDate': fromDate, 'toDate': toDate});
+});
+
+// add your routes here
+
+module.exports = router;
