9c77455f9e5220e8c0392b937811623777f4ab6a
Mark Goddard <mark.goddard@digital.homeoffice.gov.uk>
2016-03-11 16:01:45 +0000

    unsupported applicant added

--- app/routes.js
+++ app/routes.js
@@ -48,6 +48,10 @@ router.get('/income/2/results', function(req, res, next) {
     res.render('income/2/results-fail', {'global_header_text' : 'Home Office', 'nino': nino, 'toDate': humanToDate, 'fromDate': humanFromDate});
     return true
   }
+  else if(nino.length == 1 && nino == "QQ123456B"){
+    res.render('income/2/results-unsupported', {'global_header_text' : 'Home Office', 'nino': nino, 'toDate': humanToDate, 'fromDate': humanFromDate});
+    return true
+  }
   else if(nino.length == 1 && nino != "QQ123456A"){
     res.render('income/2/results-success', {'global_header_text' : 'Home Office', 'nino': nino, 'toDate': humanToDate, 'fromDate': humanFromDate});
     return true
